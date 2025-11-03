6a832bb1187769824638b79a9b16a96417eefbd3
Lindelman22 <lizzcurie7@icloud.com>
2025-11-03 10:18:30 -0600

    feature/aboutusContact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,11 +1,41 @@
+import '../contact.css';
+
 export default function Contact() {
   return (
     <>
-      <h1>Contacto</h1>
-      <h4>Información de contacto</h4>
-      <h6>Email: contacto@spikeshop.com</h6>
-      <h6>Teléfono: 123-456-7890</h6>
-      <h6>Nombre</h6>
+
+      <div className='rowContact'>
+        <div className='colContact'>
+          <div className='textoContainer'div>
+          <h1>Contacto</h1>
+          <h4>Información de contacto</h4>
+          <h6>Email: contacto@spikeshop.com</h6>
+          <h6>Teléfono: 123-456-7890</h6>
+          <h6>Nombre</h6>
+          </div>
+        </div>
+        <div className='colContact'>
+          <div className='imgContainer'>
+          </div>
+        </div>
+      </div>
+      <div className='rowContact'>
+        <div className='colContact2'>
+          <div className='imgContainer'>
+          </div>
+        </div>
+        <div className='colContact2'>
+           <div className='textoContainer'div>
+          <h1>Contacto</h1>
+          <h4>Información de contacto</h4>
+          <h6>Email: contacto@spikeshop.com</h6>
+          <h6>Teléfono: 123-456-7890</h6>
+          <h6>Nombre</h6>
+          </div>
+        </div>
+      </div>
+
+
     </>
   );
 }
