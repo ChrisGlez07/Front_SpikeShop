30816ab8f42e668176399bcdeb1d914871bd6c78
Lindelman22 <lizzcurie7@icloud.com>
2025-11-04 09:40:01 -0600

    about us update

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -7,11 +7,11 @@ export default function Contact() {
       <div className='rowContact'>
         <div className='colContact'>
           <div className='textoContainer'div>
-          <h1>Contacto</h1>
-          <h4>Información de contacto</h4>
-          <h6>Email: contacto@spikeshop.com</h6>
-          <h6>Teléfono: 123-456-7890</h6>
-          <h6>Nombre</h6>
+        
+        <h1 className='textoTitulo1'>About us</h1>
+          
+          <h4 className='paragraf1'>SpikeShop:is a 100% Mexican clothing store created with a passion for style, authenticity, and innovation.</h4>
+         <h4 className='paragraf1'>We are inspired by urban culture and modern trends, but always with a local touch that reflects our identity and creativity.</h4>
           </div>
         </div>
         <div className='colContact'>
@@ -21,16 +21,14 @@ export default function Contact() {
       </div>
       <div className='rowContact'>
         <div className='colContact2'>
-          <div className='imgContainer'>
+          <div className='imgContainer2'>
           </div>
         </div>
         <div className='colContact2'>
            <div className='textoContainer'div>
-          <h1>Contacto</h1>
-          <h4>Información de contacto</h4>
-          <h6>Email: contacto@spikeshop.com</h6>
-          <h6>Teléfono: 123-456-7890</h6>
-          <h6>Nombre</h6>
+          <h1 className='titulo2'>Our mission</h1>
+          <h4 className='paragraf2'>We are committed to offering quality products that combine design, durability, and style.</h4>
+         <h4 className='paragraf2'>We want each customer to have a unique experience when wearing our garments, promoting Mexican talent and supporting responsible consumption.</h4>
           </div>
         </div>
       </div>
